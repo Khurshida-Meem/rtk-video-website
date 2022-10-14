e65d1615dc71f8c5d73f72fa4b3e93a8ae9f301e
Khurshida-Meem <meemrumi59@gmail.com>
2022-10-14 10:28:10 +0600

    updates

--- src/components/video-grid/index.js
+++ src/components/video-grid/index.js
@@ -1,15 +1,30 @@
 import React from "react";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchVideos } from "../../features/videos/videoSlice";
 import Video from "./video";
 
 const VideoGrid = () => {
+  const dispatch = useDispatch();
+  const { videos, isLoading, isError, error } = useSelector(
+    (state) => state.videos
+  );
+
+  useEffect(() => {
+    dispatch(fetchVideos());
+  }, [dispatch]);
+
   return (
     <section className="pt-12">
       <section className="pt-12">
         <div className="grid grid-cols-12 gap-4 max-w-7xl mx-auto px-5 lg:px-0 min-h-[300px]">
-          <Video />
+          {isLoading ? (
+            <p>Loading...</p>
+          ) : (
+            videos.map((video) => <Video key={video?.id} video={video} />)
+          )}
 
-          {/* <!-- error section
-                    <div className="col-span-12">some error happened</div> --> */}
+          {isError && <div className="col-span-12">{error}</div>}
         </div>
       </section>
     </section>
